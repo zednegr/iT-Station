3032c4ce5f099e5afe927121bc5d572cdd8d46cf
zednegr <negrzed@gmail.com>
2023-09-14 00:36:52 +0500

    update

--- src/components/free_course/style.js
+++ src/components/free_course/style.js
@@ -1,160 +1,160 @@
 import { styled } from "styled-components";
 
-export const FreeCourseCardTop = styled.div``
+export const FreeCourseCardTop = styled.div``;
 
 export const FreeCourseSection = styled.div`
-    padding-bottom: 141px;
-`
+  padding-bottom: 141px;
+`;
 
 export const FreeCourseStart = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-bottom: 45px;
-    background-image: url("../../assets/svg/free-course-line.svg");
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 45px;
+  background-image: url("../../assets/svg/free-course-line.svg");
+`;
 
 export const FreeCourseStartH3 = styled.h3`
-    color: var(--brand-color-2, #FD661F);
-    text-align: center;
-    font-family: Inter;
-    font-size: 48px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 62.4px; /* 130% */
-    letter-spacing: -2.88px;
-`
+  color: var(--brand-color-2, #fd661f);
+  text-align: center;
+  font-family: Inter;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 62.4px; /* 130% */
+  letter-spacing: -2.88px;
+`;
 
 export const FreeCourseBtnWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 24px;
-    padding-bottom: 61px;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  padding-bottom: 61px;
+`;
 
 export const FreeCourseBtns = styled.button`
-    padding: 10px 20px;
-    border-radius: 5px;
-    border: 1px solid #C4C4C4;
-    background: transparent;
-    color: #818C96;
-    font-family: Raleway;
-    font-size: 12px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-    text-transform: capitalize;
-    transition: 0.2s ease-in;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: 1px solid #c4c4c4;
+  background: transparent;
+  color: #818c96;
+  font-family: Raleway;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  text-transform: capitalize;
+  transition: 0.2s ease-in;
 
-    &:hover {
-        background: #263039;
-        color: #FFF;
-    }
-`
+  &:hover {
+    background: #263039;
+    color: #fff;
+  }
+`;
 
 export const FreeCourseCardsWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 25px;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 25px;
+`;
 
 export const FreeCourseCard = styled.div`
-    width: 296px;
-    height: auto;
-    border-radius: 14px;
-    background: #FFF;
-    box-shadow: 0px 10px 60px 0px rgba(38, 45, 118, 0.08);
-`
+  width: 296px;
+  height: auto;
+  border-radius: 14px;
+  background: #fff;
+  box-shadow: 0px 10px 60px 0px rgba(38, 45, 118, 0.08);
+`;
 
 export const FreeCourseCardTopImg = styled.img`
-    border-radius: 10px;
-`
+  border-radius: 10px;
+`;
 
 export const FreeCourseCardCenter = styled.div`
-    padding: 20px;
-`
+  padding: 20px;
+`;
 
 export const FreeCourseCardCenterP = styled.p`
-    color: var(--primary-primary-3, #777795);
-    font-family: Raleway;
-    font-size: 10px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 150%; /* 15px */
-    padding-bottom: 8px;
-`
+  color: var(--primary-primary-3, #777795);
+  font-family: Raleway;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 15px */
+  padding-bottom: 8px;
+`;
 
 export const FreeCourseCardCenterH5 = styled.h5`
-    color: #263039;
-    font-family: Raleway;
-    font-size: 15.522px;
-    font-style: normal;
-    font-weight: 800;
-    line-height: 150%; /* 23.283px */
-    letter-spacing: 0.388px;
-`
+  color: #263039;
+  font-family: Raleway;
+  font-size: 15.522px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: 150%; /* 23.283px */
+  letter-spacing: 0.388px;
+`;
 
 export const FreeCourseCardCenterInfo = styled.p`
-    color: var(--grey-grey-2, #4D4D4D);
-    font-family: Raleway;
-    font-size: 10px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 160%; /* 16px */
-`
+  color: var(--grey-grey-2, #4d4d4d);
+  font-family: Raleway;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%; /* 16px */
+`;
 
 export const FreeCourseCardBottom = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 20px 20px;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px 20px;
+`;
 
 export const FreeCourseCardBottomDollarWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
 
 export const FreeCourseCardBottomH4 = styled.h4`
-    color: var(--brand-color-2, #FD661F);
-    font-family: Inter;
-    font-size: 16.681px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 150%; /* 25.022px */
-`
+  color: var(--brand-color-2, #fd661f);
+  font-family: Inter;
+  font-size: 16.681px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%; /* 25.022px */
+`;
 
 export const FreeCourseCardBottomDollar = styled.span`
-    color: var(--Black, #230F0F);
-    font-family: Inter;
-    font-size: 14.828px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 150%; /* 22.242px */
-    text-decoration: line-through;
-`
+  color: var(--Black, #230f0f);
+  font-family: Inter;
+  font-size: 14.828px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%; /* 22.242px */
+  text-decoration: line-through;
+`;
 
 export const FreeCourseCardBottomButton = styled.button`
-    padding: 10px 20px;
-    border-radius: 5px;
-    border: 1px solid #C4C4C4;
-    background: transparent;
-    color: #818C96;
-    font-family: Raleway;
-    font-size: 12px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-    text-transform: capitalize;
-    transition: 0.2s ease;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: 1px solid #c4c4c4;
+  background: transparent;
+  color: #818c96;
+  font-family: Raleway;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  text-transform: capitalize;
+  transition: 0.2s ease;
 
-    &:hover {
-        background: #263039;
-        color: #FFF;
-    }
-`
+  &:hover {
+    background: #263039;
+    color: #fff;
+  }
+`;
